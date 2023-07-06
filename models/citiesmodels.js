5dd700dd88d3a855380d65cc91727a741cdc9785
johana-GARCIAFST <jhoanagarcia674333@gmail.com>
2023-07-06 14:26:32 -0300

    espacios

--- models/citiesmodels.js
+++ models/citiesmodels.js
@@ -6,7 +6,7 @@ const citiesSchema = new mongoose.Schema({
     country:{type:String, required:true},
     description:{type:String, required:true},
     image:{type:String, required:true},
-    population:{type:Number, required:false}
+    population:{type:Number , required:false}
 
 })
 const Cities = mongoose.model("cities", citiesSchema)
